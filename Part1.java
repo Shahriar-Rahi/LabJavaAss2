06bfdc3439b2f1ac7f4cab914d5cd6e4840b5f37
Shahriar-Rahi <md.alshahriarrahi@gmail.com>
2017-09-20 23:25:52 +0600

    added testFindStopCodon & findGene

--- Part1.java
+++ Part1.java
@@ -11,4 +11,47 @@ public class Part1
         }
         return DNA.length();
     }
+    
+    void testFindStopCodon()
+    {
+        String DNA = "ATACGATATTATACCCGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+
+        DNA = "ATACGATATTATAACCCGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+        
+        DNA = "ATGACGATATTATACCCGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+        
+        DNA = "ATGACGATATAATATAACCCGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+
+        DNA = "ATGACGATAGTAATATAACCCGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+        
+        DNA = "ATGACGATAGTAATAAGTCCCTAGGAGAT";
+        System.out.println("DNA: " + DNA);
+        System.out.println("Gene: " + findGene(DNA));
+    }
+    
+        String findGene(String DNA)
+    {
+        int startIndex = DNA.indexOf("ATG");
+        if(startIndex == -1)
+            return "";
+        int index = findStopCodon(DNA, startIndex, "TAA");
+        if(index == DNA.length())
+        {
+            index = findStopCodon(DNA, startIndex, "TAG");
+        }
+        if(index == DNA.length())
+            return "";
+        else
+            return DNA.substring(startIndex, index + 3);
+    }
 }
